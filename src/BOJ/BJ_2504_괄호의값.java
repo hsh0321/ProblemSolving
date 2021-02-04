e153abfcf95c5124f5b164ea07de979da5c9ede0
hsh0321 <ppko1233@gmail.com>
2021-02-04 12:40:15 +0900

    BJ-2504-괄호의값

---
+++ src/BOJ/BJ_2504_괄호의값.java
@@ -0,0 +1,57 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BJ_2504_괄호의값 {
+
+    public static Stack<Character> stack=new Stack<Character>();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String s=br.readLine();
+        boolean f=false;
+        long ans=0;
+        int mul=1;
+
+        // (()[[]])([])
+        // (2+(3*3))*2+(2*3)
+        // (2*2)+(3*3*2)+(2*3)
+
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i)=='(') {
+                stack.add(s.charAt(i));
+                mul*=2;
+            }
+            if(s.charAt(i)=='[') {
+                stack.add(s.charAt(i));
+                mul*=3;
+            }
+            if(s.charAt(i)==')') {
+                if(stack.empty() || stack.peek()!='(') {
+                    f=true;
+                    break;
+                }if(s.charAt(i-1)=='(')
+                    ans+=mul;
+                stack.pop();
+                mul/=2;
+
+            }else if(s.charAt(i)==']') {
+                if(stack.empty() || stack.peek()!='[') {
+                    f=true;
+                    break;
+                }
+                if(s.charAt(i-1)=='[')
+                    ans+=mul;
+                stack.pop();
+                mul/=3;
+            }
+        }
+        if(f==true || !stack.empty()) {
+            System.out.println(0);
+        }else {
+            System.out.println(ans);
+        }
+    }
+
+}
---
+++ src/SWEA/SWEA_1218_괄호짝찟기.java
@@ -0,0 +1,5 @@
+package SWEA;
+
+public class SWEA_1218_괄호짝찟기 {
+
+}
