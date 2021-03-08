e17c59b814b80f68bfec2747c717aff9cd3b4714
hsh0321 <ppko1233@gmail.com>
2021-03-08 13:44:25 +0900

    BJ_16234_인구이동

---
+++ src/BOJ/BJ_16234_인구이동.java
@@ -0,0 +1,13 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_16234_인구이동 {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N,L,R;
+
+    }
+}
