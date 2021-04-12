27bf1146494541567aa34212d3899743068c9228
hsh0321 <ppko1233@gmail.com>
2021-04-12 14:17:35 +0900

    DOCS : 0412

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,54 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Scanner;
 
+import java.io.*;
+import java.util.*;
 public class Main {
-    static String num[] = {"zero","one","two","three","four","five" // 미리 저장할 숫자 영어
-                         ,"six","seven","eight","nine"};
-
-    static class Number{ // 숫자와 변환한 값을 저장할 클래스
-        int x; // 숫자
-        String s; // 변환된 숫자 영어 문자열
-        public Number(int x, String s) { // 생성자
-            this.x = x;
-            this.s = s;
-        }
-    }
-
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in); // 스캐너
-        int M,N; // M,N 선언
-        M = sc.nextInt(); // M 입력
-        N = sc.nextInt(); // N 입력
-
-        ArrayList<Number> list = new ArrayList<>(); // 숫자들을 담을 List
-        StringBuilder sb,tmp; // 숫자와 문자
-        for(int i=M;i<=N;i++){
-            sb = new StringBuilder(Integer.toString(i)); // 숫자를 string으로 저장
-            tmp = new StringBuilder(); // 숫자를 영어로 변환
-            for(int j=0;j<sb.length();j++){ // 숫자 길이만큼 반복
-                tmp.append(num[sb.charAt(j)-'0']).append(' '); // 미리 저장한 영어 배열을 사용하여 영어 완성
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder arr, answer = new StringBuilder();
+        StringTokenizer st;
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        String p;
+        int n, T = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < T; tc++) {
+            deque.clear();
+            p = br.readLine();
+            n = Integer.parseInt(br.readLine());
+            arr = new StringBuilder(br.readLine());
+            st = new StringTokenizer(arr.substring(1, arr.length() - 1), ",");
+            while (st.hasMoreTokens())
+                deque.addLast(Integer.parseInt(st.nextToken()));
+            boolean error = false;
+            int r_cnt = 0, length = p.length();
+            for (int i = 0; i < length; i++) {
+                if (p.charAt(i) == 'R') {
+                    if (i > 0 && p.charAt(i - 1) == 'D') r_cnt = 0;
+                    r_cnt++;
+                } else {
+                    if (deque.isEmpty()) {
+                        error = true;
+                        break;
+                    }
+                    if (r_cnt % 2 == 0)
+                        deque.pollFirst();
+                    else
+                        deque.pollLast();
+                }
             }
-            list.add(new Number(i,tmp.toString())); // 리스트에 추가
-        }
-
-        Collections.sort(list, new Comparator<Number>() { // string을 기준으로 정렬
-            @Override
-            public int compare(Number o1, Number o2) {
-                return o1.s.compareTo(o2.s); // string compareTo 함수를 사용
+            if (error) answer.append("error").append("\n");
+            else {
+                int size = deque.size();
+                answer.append("[");
+                if (r_cnt % 2 == 0)
+                    while (!deque.isEmpty())
+                        answer.append(deque.pollFirst()).append(",");
+                else
+                    while (!deque.isEmpty())
+                        answer.append(deque.pollLast()).append(",");
+                if (size > 0)
+                    answer.deleteCharAt(answer.length() - 1);
+                answer.append("]").append("\n");
             }
-        });
-
-        StringBuilder answer = new StringBuilder(); // 정답
-        for(int i=1;i<=list.size();i++){ // List 사이즈 만큼 반복
-            answer.append(list.get(i-1).x).append(' ');
-            if(i != 0 && i%10==0)answer.append('\n'); // 10개 단위로 줄바꿈
         }
-
-        System.out.println(answer);
+        System.out.print(answer);
     }
 }
---
+++ src/SWEA/SWEA_5644_무선충전.java
@@ -0,0 +1,136 @@
+package SWEA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class SWEA_5644_무선충전 {
+    static int test_case;
+    static int M,A;
+
+    static BufferedReader bf;
+    static StringTokenizer st;
+    static int[] dr = {0,-1,0,1,0},dc = {0,0,1,0,-1};
+    static ArrayList<Integer>[][] map = new ArrayList[10][10];
+    static Point[][] loc;
+    static boolean[][][] visit;
+    static int[] power;
+
+    static class Point{
+        int r,c;
+
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        bf = new BufferedReader(new InputStreamReader(System.in));
+        test_case = Integer.parseInt(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int t=1;t<=test_case;t++){
+            input();
+            int allSum = 0;
+            for(int i=0;i<=M;i++){
+                int tmp1=0,tmp2=0;
+                //System.out.print(i + "초 : " + loc[i][0].r + ","+loc[i][0].c + " /// ");
+
+                int sum = 0;
+                int aNum,bNum; // 사용하는 충전기
+                int tmp;
+                for(int a =0;a<map[loc[i][0].r][loc[i][0].c].size();a++){
+                    aNum = map[loc[i][0].r][loc[i][0].c].get(a);
+                    for(int b=0;b<map[loc[i][1].r][loc[i][1].c].size();b++){
+                        bNum = map[loc[i][1].r][loc[i][1].c].get(b);
+                        tmp = power[aNum] + power[bNum];
+                        if(aNum == bNum)tmp /= 2;
+                        sum = Math.max(sum,tmp);
+                    }
+                }
+                //System.out.println(sum);
+                allSum += sum;
+//                if(!map[loc[i][0].r][loc[i][0].c].isEmpty())tmp1 = map[loc[i][0].r][loc[i][0].c].get(0);
+//                if(!map[loc[i][1].r][loc[i][1].c].isEmpty())tmp2 = map[loc[i][1].r][loc[i][1].c].get(0);
+//                System.out.println(tmp1 + "," + tmp2);
+            }
+            System.out.println("#" + t + " " + allSum);
+        }
+    }
+
+    static void input() throws IOException {
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                map[i][j] = new ArrayList<>();
+                map[i][j].add(0);
+            }
+        }
+
+        st = new StringTokenizer(bf.readLine());
+        M = Integer.parseInt(st.nextToken());
+        A = Integer.parseInt(st.nextToken());
+
+        loc = new Point[M+1][2]; // a,b의 위치 [T][0] : A, [T][1] : B
+        Point aLoc = new Point(0,0);
+        Point bLoc = new Point(9,9);
+        st = new StringTokenizer(bf.readLine());
+        loc[0][0] = new Point(0,0);
+        for(int i=1;i<=M;i++){ // a 좌표
+            int dir = Integer.parseInt(st.nextToken());
+            aLoc.r += dr[dir];
+            aLoc.c += dc[dir];
+            loc[i][0] = new Point(aLoc.r,aLoc.c);
+        }
+
+        st = new StringTokenizer(bf.readLine());
+        loc[0][1] = new Point(9,9);
+        for(int i=1;i<=M;i++){ // a 좌표
+            int dir = Integer.parseInt(st.nextToken());
+            bLoc.r += dr[dir];
+            bLoc.c += dc[dir];
+            loc[i][1] = new Point(bLoc.r,bLoc.c);
+        }
+        // AP
+        visit = new boolean[10][10][A+1];
+        power = new int[A+1];
+        for(int i=1;i<=A;i++) {
+            st = new StringTokenizer(bf.readLine());
+            int x,y,c,p;
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+            p = Integer.parseInt(st.nextToken());
+            chargeArea(i,new Point(y-1,x-1),c);
+            power[i] = p;
+        }
+    }
+
+    static void chargeArea(int num,Point p,int area){
+        Queue<Point> q = new LinkedList<>();
+        q.offer(p);
+        map[p.r][p.c].add(num);
+        int cnt = 0;
+        while(cnt != area){
+            int size = q.size();
+            while(size-- != 0){
+                Point cur = q.poll();
+                for(int i=1;i<5;i++){
+                    Point nxt = new Point(cur.r,cur.c);
+                    nxt.r += dr[i];
+                    nxt.c += dc[i];
+                    if(nxt.r < 0 || nxt.r >= 10 || nxt.c < 0 || nxt.c >= 10)continue;
+                    if(visit[nxt.r][nxt.c][num])continue;
+                    visit[nxt.r][nxt.c][num] = true;
+                    map[nxt.r][nxt.c].add(num); // ac추가
+                    q.offer(nxt);
+                }
+            }
+            cnt++;
+        }
+    }
+}
